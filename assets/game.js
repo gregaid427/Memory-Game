48f48ecf8d34fcaf8b777ea2e1af31e30779c293
Gregory Aidoo <gregoryd428@gmail.com>
2022-12-30 13:57:07 +0000

    Update game.js

--- assets/game.js
+++ assets/game.js
@@ -367,7 +367,7 @@ const flipCard = card => {
                    
                     <div class="two mt">
                       <button class="reset" onclick="pageloader()" >Restart</button>
-                      <a href="index.php"> <button class="dark" >Set Up New Game</button></a>
+                      <a href="index.html"> <button class="dark" >Set Up New Game</button></a>
                     </div>
                   </div>
                   </div>
@@ -413,7 +413,7 @@ const flipCard = card => {
           </div>
                 <div class="two mt">
                   <button class="reset" onclick="pageloader()" >Restart</button>
-                  <a href="index.php"> <button class="dark" >Set Up New Game</button></a>
+                  <a href="index.html"> <button class="dark" >Set Up New Game</button></a>
                 </div>
               </div>
               </div>
